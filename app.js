abbc06478d9ded05080ebdaac9a149788272ab31
Filip Glišović <124403806+glisovic01@users.noreply.github.com>
2023-02-04 13:25:43 +0100

    Added new files

---
+++ app.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById("navLinks");
+
+/* Open */
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+/* Close */
+function hideMenu() {
+  navLinks.style.right = "-70vw";
+}
